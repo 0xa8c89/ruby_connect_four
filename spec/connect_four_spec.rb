32576f158c31a43804437ea8a928546e8372be67
0xa8c89 <117485263+0xa8c89@users.noreply.github.com>
2023-01-31 03:24:01 +0100

    add test for initialize

--- spec/connect_four_spec.rb
+++ spec/connect_four_spec.rb
@@ -1,13 +1,15 @@
 require_relative '../lib/main'
 
-describe ConnectFour do
+describe Board do
   describe '#initialize' do
-    let(:board) { double(board: Array.new(7) { Array.new(6) { ' ' } }) }
+    black_circle = "\u25CF" # can be colorized with black_circle.colorize(:red)
+    white_circle = "\u25CB"
+    let(:board) { Array.new(6) { Array.new(7) { white_circle } } }
 
-    it 'creates a 7 x 6 array' do
-      p board.board
-      expect(board.board.length).to eq(7)
-      expect(board.board[0].length).to eq(6)
+    it 'creates 6 arrays of length 7' do
+      print board
+      expect(board.length).to eq(6)
+      expect(board[0].length).to eq(7)
     end
   end
 end
