ed696c2335ca9261e847b690d4b5bfe2b3e545f3
0xa8c89 <117485263+0xa8c89@users.noreply.github.com>
2023-02-05 04:24:33 +0100

    move let to class context

--- spec/connect_four_spec.rb
+++ spec/connect_four_spec.rb
@@ -4,11 +4,10 @@ black_circle = "\u25CF" # can be colorized with black_circle.colorize(:red)
 white_circle = "\u25CB"
 
 describe Board do
-  describe '#initialize' do
-    let(:board) { Array.new(6) { Array.new(7) { white_circle } } }
+  let(:board) { Array.new(6) { Array.new(7) { white_circle } } }
 
+  describe '#initialize' do
     it 'creates 6 arrays of length 7' do
-      print board
       expect(board.length).to eq(6)
       expect(board[0].length).to eq(7)
     end
